a3b72c3ccec35ba1ddcd2974b9b666521be675da
jemin <rudanijemin18@gmail.com>
2021-07-01 18:33:43 +0530

    add music icon

---
+++ lib/main.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+ class MyApp extends StatelessWidget {
+
+   @override
+   Widget build(BuildContext context) {
+     return MaterialApp(
+       debugShowCheckedModeBanner: false,
+       home: MusicApp(),
+     );
+   }
+ }
+
+ class MusicApp extends StatefulWidget {
+
+   @override
+   _MusicAppState createState() => _MusicAppState();
+ }
+
+ class _MusicAppState extends State<MusicApp> {
+   @override
+   Widget build(BuildContext context) {
+     return Scaffold(
+       body: Container(
+         width: double.infinity,
+         decoration: BoxDecoration(
+           gradient: LinearGradient(
+             begin: Alignment.topLeft,
+             end: Alignment.bottomRight,
+             colors: [
+               Colors.black,
+               Colors.black87,
+             ]
+           ),
+         ),
+         child: Padding(
+           padding: EdgeInsets.symmetric(vertical: 60,horizontal: 20),
+           child: Container(
+             child: Column(
+               mainAxisAlignment: MainAxisAlignment.start,
+               crossAxisAlignment: CrossAxisAlignment.start,
+               children: [
+                 Text("Music Beats",
+                   style:TextStyle(
+                     color: Colors.blueGrey,
+                     fontSize: 35,
+                     fontWeight: FontWeight.bold,
+                   ),
+                 ),
+                 Text("List to your Favorite Song",
+                   style:TextStyle(
+                     color: Colors.blueGrey,
+                     fontSize: 20,
+                     fontWeight: FontWeight.w500,
+                   ),
+                 ),
+                 SizedBox(
+                   height: 25,
+                 ),
+
+                 Center(
+                   child: Container(
+                     width: 250,
+                     height: 200,
+                     decoration: BoxDecoration(
+                       borderRadius: BorderRadius.circular(60),
+                       image: DecorationImage(
+                         image: AssetImage("images/music.jpg"),
+                       ),
+                     ),
+                   ),
+                 ),
+                 SizedBox(
+                   height: 18,
+                 ),
+                 Center(
+                   child: Text("Stargazer",
+                     style: TextStyle(
+                       color: Colors.blueGrey,
+                       fontSize: 25,
+                     ),
+                   ),
+
+                 ),
+                 SizedBox(height: 30),
+                 Expanded(child:Column(
+                   mainAxisAlignment: MainAxisAlignment.center,
+                   crossAxisAlignment: CrossAxisAlignment.center,
+                   
+
+                 ) ),
+               ],
+             ),
+           ),
+         ),
+
+       ),
+
+     );
+   }
+ }
+
+ 
